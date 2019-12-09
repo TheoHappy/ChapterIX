3acf39fca9e26903911add37786c1b5b855afa6d
TheoHappy <teodore473@gmail.com>
2019-12-09 13:53:19 +0200

    First commit

---
+++ src/CreatingANewSubclassOfEmployee/HourlyEmployeeTest.java
@@ -0,0 +1,8 @@
+package CreatingANewSubclassOfEmployee;
+
+public class HourlyEmployeeTest {
+    public static void main(String[] args) {
+        HourlyEmployee employee = new HourlyEmployee("Andrei", "Moraru","333-4352-235",168,10);
+        System.out.println(employee);
+    }
+}
---
+++ src/EmployeeHierarchy/CommissionEmployee.java
@@ -0,0 +1,48 @@
+package EmployeeHierarchy;
+
+import CreatingANewSubclassOfEmployee.Employee;
+
+public class CommissionEmployee extends Employee {
+    private double grossSales; // gross weekly sales
+    private double commissionRate; // commission percentage
+
+
+    public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate) {
+        super(firstName, lastName, socialSecurityNumber);
+        setGrossSales(grossSales);
+        setCommissionRate(commissionRate);
+    }
+
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    public void setGrossSales(double grossSales) {
+        if (grossSales < 0.0)
+            throw new IllegalArgumentException(
+                    "Gross sales must be >= 0.0");
+
+        this.grossSales = grossSales;
+    }
+
+    public double getCommissionRate() {
+        if (commissionRate <= 0.0 || commissionRate >= 1.0)
+            throw new IllegalArgumentException(
+                    "Commission rate must be > 0.0 and < 1.0");
+
+        return commissionRate;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        this.commissionRate = commissionRate;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "grossSales = " + grossSales + "\n" +
+                "commissionRate = " + commissionRate + "\n";
+
+    }
+
+}
